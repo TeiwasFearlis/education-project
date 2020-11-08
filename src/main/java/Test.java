ab7ef3f9fbfccf20a873b4b664808a9531341440
COMPUTER\Anton <teiwas88@rambler.ru>
2020-11-08 19:15:08 +0400

    clean up

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,6 @@
 
 public class Test {
     public static void main(String[] args) {
-        String str = "Hello world";
         String str2 = "Not Hello world";
         System.out.println(str2);
     }
